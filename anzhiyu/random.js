03d3a8cc81acb224e3a8f67da6c3b6f640f628ab
LD2J0618 <mxz@ld2j.top>
2024-12-05 00:34:30 +0800

    Site updated: 2024-12-05 00:34:30

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["essay/教程开始前的一些有用的话/","essay/hello-world/"];function toRandomPost(){
+var posts=["essay/教程开始前的一些有用的话/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
