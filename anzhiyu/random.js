d4b33cb294c285f74011f9f5412575d791ce1ed9
LD2J0618 <mxz@ld2j.top>
2024-12-04 23:21:33 +0800

    Site updated: 2024-12-04 23:21:33

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/12/04/hello-world/"];function toRandomPost(){
+var posts=["2024/12/04/hello-world/","2024/12/04/教程开始前的一些有用的话/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
