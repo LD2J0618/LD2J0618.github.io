5cad0d79cd0fdef70a16ac9073220e43e611aa58
LD2J0618 <mxz@ld2j.top>
2024-12-05 15:47:30 +0800

    Site updated: 2024-12-05 15:47:30

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["essay/教程开始前的一些有用的话/"];function toRandomPost(){
+var posts=["essay/教程开始前的一些有用的话/","essay/Loon：新手从入门到卸载/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
