52ad1e467b5a9e77a0bef2ee6ab9824660728d78
LD2J0618 <mxz@ld2j.top>
2024-12-07 19:36:47 +0800

    Site updated: 2024-12-07 19:36:46

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["essay/Loon：新手从入门到卸载/","essay/教程开始前的一些有用的话/"];function toRandomPost(){
+var posts=["essay/教程开始前的一些有用的话/","essay/Loon：新手从入门到卸载/","essay/如何在vps上部署Bark通知ssh连接及资源监控脚本/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
